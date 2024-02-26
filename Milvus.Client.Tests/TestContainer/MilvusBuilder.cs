15dc270c0e466c0ab2c1caead382f140acb19a04
Shay Rojansky <roji@roji.org>
2024-02-27 00:43:55 +0100

    Abortive attempt to run tests against 2.2
    There are big differences; it won't be trivial to get 2.2 to work.

--- Milvus.Client.Tests/TestContainer/MilvusBuilder.cs
+++ Milvus.Client.Tests/TestContainer/MilvusBuilder.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using System.Text;
 using Docker.DotNet.Models;
 using DotNet.Testcontainers.Builders;
@@ -8,7 +7,10 @@ namespace Milvus.Client.Tests.TestContainer;
 
 public class MilvusBuilder : ContainerBuilder<MilvusBuilder, MilvusContainer, MilvusConfiguration>
 {
-    public const string MilvusImage = "milvusdb/milvus:v2.3.0"; // TODO: Configurable
+    public const string DefaultImageTag = "v2.2.16";
+    // public const string DefaultImageTag = "v2.3.0";
+
+    public const string MilvusImage = "milvusdb/milvus";
     public const ushort MilvusGrpcPort = 19530;
     public const ushort MilvusManagementPort = 9091;
 
@@ -31,8 +33,12 @@ public class MilvusBuilder : ContainerBuilder<MilvusBuilder, MilvusContainer, Mi
                        advertise-client-urls: http://0.0.0.0:2379
                        """;
 
+        string fullImage = MilvusImage + ":" +
+                           (Environment.GetEnvironmentVariable("MILVUS_IMAGE_TAG") ?? DefaultImageTag);
+
+        // milvusdb/milvus:v2.2.16
         return base.Init()
-            .WithImage(MilvusImage)
+            .WithImage(fullImage)
             .WithEnvironment("COMMON_STORAGETYPE", "local")
             .WithEnvironment("ETCD_USE_EMBED", "true")
             .WithEnvironment("ETCD_DATA_DIR", "/var/lib/milvus/etcd")
@@ -41,11 +47,17 @@ public class MilvusBuilder : ContainerBuilder<MilvusBuilder, MilvusContainer, Mi
             .WithCommand("milvus", "run", "standalone")
             .WithPortBinding(MilvusGrpcPort, true)
             .WithPortBinding(MilvusManagementPort, true)
+            // For 2.3.x:
             .WithWaitStrategy(
                 Wait.ForUnixContainer()
-                    .UntilHttpRequestIsSucceeded(h => h
-                        .ForPort(MilvusManagementPort)
-                        .ForPath("/healthz")));
+                    .UntilContainerIsHealthy());
+        // .UntilPortIsAvailable(MilvusGrpcPort));
+
+        // .WithWaitStrategy(
+        //     Wait.ForUnixContainer()
+        //         .UntilHttpRequestIsSucceeded(h => h
+        //             .ForPort(MilvusManagementPort)
+        //             .ForPath("/healthz")));
     }
 
     protected override MilvusBuilder Clone(IResourceConfiguration<CreateContainerParameters> resourceConfiguration)
